e26b1032ffa7c3c49bb50c58140fd8b807484265
Arzed Five <rz5@users.noreply.github.com>
2016-04-28 13:41:06 +0100

    Translating buffer.rs. I hit a conceptual snag because of Rust's PhantomData structure

--- rustation-libretro/src/retrogl/buffer.h
+++ rustation-libretro/src/retrogl/buffer.h
@@ -1,6 +1,33 @@
 #ifndef RETROGL_BUFFER_H
 #define RETROGL_BUFFER_H
 
+#include <gl>
+#include <stdlib.h> // size_t
+
+#include "error.h"
+#include "vertex.h"
+#include "program.h"
+#include "types.h"
+
+template<typename T>
+class DrawBuffer<T> 
+{
+public:
+    /// OpenGL name for this buffer
+    GLuint id;
+    /// Vertex Array Object containing the bindings for this
+    /// buffer. I'm assuming that each VAO will only use a single
+    /// buffer for simplicity.
+    VertexArrayObject vao;
+    /// Program used to draw this buffer
+    Program program;
+    // Number of elements T that the vertex buffer can hold
+    size_t capacity;
+    /// Marker for the type of our buffer's contents
+    /* How do I simulate this in C++? Maybe an array of size 1? */
+    PhantomData<T> contains;
+
+};
 
 
 #endif
